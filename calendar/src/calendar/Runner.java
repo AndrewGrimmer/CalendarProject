36ebacfdf3c770faaf74befde96314a299eb0928
AndrewGrimmer <37918240+AndrewGrimmer@users.noreply.github.com>
2018-03-29 20:24:18 -0600

    Add files via upload

---
+++ calendar/src/calendar/Runner.java
@@ -0,0 +1,10 @@
+package calendar;
+
+public class Runner 
+{
+	public static void main (String[] args)
+	{
+		CalendarProject PlannerAppController = new CalendarProject();
+		PlannerAppController.start();
+	}
+}
